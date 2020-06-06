b2bac47feff793333b2f4b14e49b68162e095c58
Whatley, Julio (jw970q) <jw970q@att.com>
2020-06-06 13:28:57 -0400

    Adding server support

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+app.use(express.static('./dist/project-ios-web'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', {root: 'dist/project-ios-web/'});
 });
 
 // Start the app by listening on the default Heroku port
